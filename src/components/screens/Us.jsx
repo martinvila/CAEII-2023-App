c21236c0ede7c3d39a7720b7ba72814b16fe8be4
juanconcep0770 <juanconcep0770@gmail.com>
2023-06-19 21:01:34 -0300

    pruebaus

--- src/components/screens/Us.jsx
+++ src/components/screens/Us.jsx
@@ -5,7 +5,7 @@ const Nosotros = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere incidunt aspernatur, nulla, rem animi fuga non dolore pariatur vel quibusdam aliquam sapiente veritatis. Hic veniam quas eum nostrum tenetur aliquam iure quae incidunt illum? Temporibus expedita reiciendis asperiores qui quisquam accusantium quos. Recusandae minima voluptate commodi reprehenderit quod non dignissimos!
+                francis se hace el ingles con us 
             </Text>
         </View>
     );
